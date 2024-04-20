b8f1be1f95160409201b06c5b2fb82fa9273daae
KindaSloth <guilhermereis28122002@gmail.com>
2024-04-20 17:35:42 -0300

    feat: format & add hardware-configuration file

--- configuration.nix
+++ configuration.nix
@@ -102,10 +102,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  	git
-  	vim
-  	wget
-  	curl
+    git
+    vim
+    wget
+    curl
     microsoft-edge
   ];
 
