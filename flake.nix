d08abd1c6e596e9a7db302f8b07a241f47c10312
KindaSloth <guilhermereis28122002@gmail.com>
2024-04-20 17:17:19 -0300

    feat: initial config

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "My NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+     url = "github:nix-community/home-manager/release-23.11";
+     inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = { 
+      nixos = nixpkgs.lib.nixosSystem { 
+        system = "x86-64-linux";
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+       
+            home-manager.users.kindasilly = import ./kindasilly/home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ kindasilly/home.nix
@@ -0,0 +1,119 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "kindasilly";
+  home.homeDirectory = "/home/kindasilly";
+
+  home.packages = with pkgs; [
+    # personal usage
+    discord
+
+    # command line tools
+    neofetch
+    nnn
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep
+    jq
+    eza
+    fzf
+
+    # networking tools
+    mtr
+    iperf3
+    dnsutils
+    ldns
+    aria2
+    socat
+    nmap
+    ipcalc
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    zstd
+    gnupg
+
+    # nix
+    nix-output-monitor
+
+    # productivity
+    glow
+    btop
+    iotop
+    iftop
+
+    # development
+    insomnia
+    docker
+    docker-compose
+
+    # system call monitoring
+    strace
+    ltrace
+    lsof
+
+    # system tools
+    sysstat
+    lm_sensors
+    pciutils
+    usbutils
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "KindaSloth";
+    userEmail = "guilhermereis28122002@gmail.com";
+  };
+
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+    history.size = 10000;
+    history.path = "${config.xdg.dataHome}/zsh/history";
+  
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "z" ];
+      theme = "robbyrussell";
+    };
+  };
+
+  programs.vscode = {
+    enable = true;
+  };
+
+  home.stateVersion = "23.11";
+
+  programs.home-manager.enable = true;
+}
